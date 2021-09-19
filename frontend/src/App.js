48656f51aee1b4ef5364cff519eed58bb404825e
Youbin Roh <yroh@hawk.iit.edu>
2021-09-19 02:07:28 -0500

    Changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
-import {GoogleMap} from "react-google-maps"
-import withScriptjs from 'react-google-maps/lib/withScriptjs';
-import withGoogleMap from 'react-google-maps/lib/withGoogleMap';
 import React from 'react';
 
-function Map() {
-  return (
-    <GoogleMap defaultZoom={10} defaultCenter={{lat: 43.073051, lng: -89.401230}}/>
-  );
-}
+class App extends React.Component {
 
-const Wrapper = withScriptjs(withGoogleMap(Map));
+  constructor() {
+    super();
+    this.state = {
+      list: {},
+      loading: true
+    };
+  }
+  
+  componentDidMount() {
+    fetch('http://localhost:8000/api/get_users')
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          list: json,
+          loading: false
+        })
+      });
+  }
 
-function App() {
-  return (
-    <div style = {{width: '50vw', height: '50vh', margin:100}}>
-      <form>
-        <h1>Find start ups near you!</h1>
-        <p>Distance from you to startup:</p>
-        <input
-          type="text" id="distance"
-        />
-      </form>
-      <Wrapper 
-        googleMapURL={'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyCv5Omy7A_HGuo64G5tWyqf9Ka8HqKHIEc' }
-        loadingElement = {<div style={{ height: "100%"}} />}
-        containerElement = {<div style={{ height: "100%"}} />}
-        mapElement = {<div style={{ height: "100%"}} />}
-      />
-    </div>
-  );
+  render() {
+    if (this.state.loading) {
+      return 'Loading...'
+    }
+
+    return (
+      <div style = {{width: '50vw', height: '50vh', margin:100}}>
+        <form>
+          <h1>Find start ups near you!</h1>
+          <p>Distance from you to startup:</p>
+          <input
+            type="text" id="distance"
+          />
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
--- rest_api/rest_api/settings.py
+++ rest_api/rest_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'e_gxp$#=_bqusi(e!+r2t%#793auy#hct5h8x94d3o3(1wi^=2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -39,9 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'api',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -124,3 +126,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_ALLOW_ALL = True
